4cc503e706fd69340d579bb92270a101c52533db
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-16 17:14:48 -0300

    Create SitioReciclaje.cs
    Nos interesa saber su ubicación, al igual que el cuartel, para poder mandar a nuestros operadores para recargar batería.

---
+++ TP_Integrador/SitioReciclaje.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class SitioReciclaje : Terreno
+    {
+        private int fila, columna;
+        public SitioReciclaje(int f, int c) { 
+            this.fila = f;
+            this.columna = c;
+        }
+    }
+}
