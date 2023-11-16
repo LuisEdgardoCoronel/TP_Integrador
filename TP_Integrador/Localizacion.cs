c7a4128c3e6cee97c71bad00d4d6a133836c49d4
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-15 22:33:22 -0300

    Create Localizacion.cs
    Clase Localizacion, utilizada para poder determinar las ubicaciones especificas, tanto de los operadores, como de los cuarteles y centros de reciclase (importantes estos 2 últimos, ya que realizan tareas especificas y nos interesa conocer su localización).

---
+++ TP_Integrador/Localizacion.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class Localizacion
+    {
+        private int fila,columna;
+
+        public Localizacion(int f, int c) {
+            this.fila = f;
+            this.columna = c;
+        }
+
+         public int getFila() { return fila; }
+
+         public void setFila(int fila)
+        {
+            this.fila = fila;
+        }
+        public int getColumna() { return columna;}
+
+        public void setColumna(int columna)
+        {
+            this.columna = columna;
+        }
+
+
+
+    }
+}
