088b501943c888d9876d4f89a973c561dcdd3880
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-16 17:18:02 -0300

    Update Cuartel.cs
    Se realizaron cambios sobre las localizaciones y otras. Están comentados lo cambios.

--- TP_Integrador/Cuartel.cs
+++ TP_Integrador/Cuartel.cs
@@ -1,23 +1,26 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
-    internal class Cuartel
+    internal class Cuartel : Terreno
     {
        private List<Operador> Operadores;
-       private String localizacion;
+       private Localizacion localizacion;
 
    
-      public Cuartel(String localizacion) {       // Constructor
-          this.localizacion = localizacion;
+      public Cuartel(int fila,int columna) {       // Constructor
+          this.localizacion = new Localizacion(fila,columna);
           this.Operadores = new List<Operador>();
         }
       
+
+       
 
       public void estado()                      //Muestra el estado actual de todos los operadores del cuartel
         {
@@ -28,12 +31,12 @@ namespace TP_Integrador
             }
         }
 
-      public void estado (String Localizacion)  //Muestra el estado actual de los operadores que se encuentran en una det. localizacion
+      public void estado (Localizacion localizacion)  //Muestra el estado actual de los operadores que se encuentran en una det. localizacion
         {
               for (int i = 0;i < this.Operadores.Count;i++)
             {
                 Operador op = this.Operadores[i];
-                if (op.getLocalizacion().CompareTo(Localizacion) ==0)
+                if (op.getLocalizacion().equals(localizacion)) ///Modificado comparacion de localizaciones
                 {
                     Console.WriteLine($"Operador con Id: [{op.getId}]. Estado = {op.getEstado}");
                 }
@@ -61,13 +64,13 @@ namespace TP_Integrador
             else Console.WriteLine("No se encontro Operador con Id ["+Id+"]");
         }
 
-        public void enviarOperador(int Id, String localizacion)   //Envia a un operador especifico a una det. localizacion
+        public void enviarOperador(int Id, Localizacion localizacion)   //Envia a un operador especifico a una det. localizacion
         {
             int posicion = estaEnLista(Id);
 
             if (posicion >= 0)
             {
-                this.Operadores[posicion].moverse(localizacion);
+                this.Operadores[posicion].moverse(localizacion); ///Modificacion, cambiamos el string por la clase localizacion
             }
             else
             {
@@ -82,7 +85,7 @@ namespace TP_Integrador
 
             if (posicion >= 0)
             {
-                this.Operadores[posicion].setEstado(EstadoOperador.StandBy);
+                this.Operadores[posicion].setEstado("StandBy");
             }
             else
             {
@@ -98,33 +101,28 @@ namespace TP_Integrador
             Operador op = cargarOperador();
             if (op != null)
             {
-                op.setCuartel(this.localizacion);
+ 
                 this.Operadores.Add(op);
             }
             else Console.WriteLine("No se pudo crear el operador.");
         }
 
-        public void agregarOperador(Operador op)          //Agrega un operador existente a las lista de operadores del Cuartel
-        {
-            op.setCuartel(this.localizacion);
-            this.Operadores.Add(op);
-        }
+        ///Se elimino el segundo metodo, ya que no era necesario. Por que todos los operadores se crean en un Cuartel especifico
 
         private Operador cargarOperador()                //Metodo privado encargado de crear un nuevo Operador
         {
             Operador op = null;
-            String localizacion;
+            
             short tipo = tipoOperador();
-            Console.WriteLine("Indique la localizacion actual del Operador: ");
-            localizacion = Console.ReadLine();
+           
 
             switch (tipo)
             {
-                case 1: op = new UAV(localizacion);
+                case 1: op = new UAV(this.localizacion);  ///Recibe la localizacion del cuartel. Idem para los demas
                     break;
-                case 2: op = new M8(localizacion);
+                case 2: op = new M8(this.localizacion);
                     break;
-                case 3: op = new K9(localizacion);
+                case 3: op = new K9(this.localizacion);
                     break;
                 default: Console.WriteLine("Error. No se eligio tipo de Operador.");
                     break;
@@ -173,10 +171,10 @@ namespace TP_Integrador
 
         }
 
-
-
-
-
+        public Localizacion GetLocalizacion()  ///Agregue metodo para obtener localizacion del cuartel
+        {                                      ///No se agregar a la clase terreno pq no nos interesa guardar la localizacion
+            return this.localizacion;         ///De los demas terrenos
+        }
 
 
     }
