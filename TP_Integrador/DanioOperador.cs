fe380d6203cf861dc753030e9689b5ea42b31b3d
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-11-16 21:10:53 -0300

    Create DanioOperador.cs
    Clase con los daños posibles de un operador

---
+++ TP_Integrador/DanioOperador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class DanioOperador
+    {
+        private static List<IDanioOperador> Danios;
+        
+        static DanioOperador()
+        {
+            Danios = new List<IDanioOperador>
+            {
+                new MotorComprometido(),
+                new ServoAtascado(),
+                new BateriaPerforada(),
+                new PuertoDesconectado(),
+                new PinturaRayada()
+            };
+        }
+        
+
+        public static IDanioOperador DanioAleatorio()
+        {
+            Random randon= new();
+            int indice = randon.Next(Danios.Count);
+            return Danios[indice];
+        }
+    }
+}
