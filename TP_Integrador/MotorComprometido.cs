62828476877e2eef3d541885ba66b61574dec84d
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-18 23:32:31 -0300

    Create MotorComprometido.cs

---
+++ TP_Integrador/MotorComprometido.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class MotorComprometido : IDanioOperador
+    {
+        public void ProducirDanio(Operador operador)
+        {
+            double vel = operador.GetVelocidad();
+            operador.SetVelocidad(vel / 2);
+        }
+    }
+}
