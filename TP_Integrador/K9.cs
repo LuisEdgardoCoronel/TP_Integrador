1a6b99dbc8e293dd2b0010094a0605ccde0f66df
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-19 16:36:32 -0300

    Update K9.cs
    Se agregaron nuevos parámetros en el constructor, ya que al crear cada operador se recibe un mapa determinado, además del tipo de operador: terrestre o aéreo.

--- TP_Integrador/K9.cs
+++ TP_Integrador/K9.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 namespace TP_Integrador
 {
     internal class K9 : Operador
-    {
-        public K9(Localizacion localizacion) : base(localizacion)
+    { 
+       
+        public K9(Localizacion localizacion,MapaTerrestre mapa,String tipo) : base(localizacion)
         {
             this.Bateria = new Bateria(TamañoBateria.mediana);
             this.CargaMax = (int)CargaOperador.Media;
             this.velocidad = 100;
-
+            this.mapa = mapa; ///Se le da un mapa a nuestro operador
+            this.tipo = tipo;
         }
     }
 }
