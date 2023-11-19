add000e2810a43dc2a59ee52b02f6e0dc703108d
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-18 23:38:32 -0300

    Create TamañoBateria.cs
    Se cambio el nombre del Enum "CargaBateria" por "TamañoBateria".

---
+++ TP_Integrador/TamañoBateria.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    public enum TamañoBateria
+    {//no se me ocurria que otro nombre ponerle a las capacidades
+        pequeña = 4000,
+        mediana = 6500,
+        grande = 12250
+    }
+}
