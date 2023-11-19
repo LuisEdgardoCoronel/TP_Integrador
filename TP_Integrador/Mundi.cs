8d254bd05cd0eba7df7364d60b5487ecfc550e0f
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-19 16:45:01 -0300

    Update Mundi.cs
    Se eliminaron algunos atributos no necesarios y redundantes.
    Luego de crear el mundo, en el método "cargarMundi", se crean mapas terrestre y aéreo, para asignárselos a cada uno de los cuarteles que se hayan creado.

--- TP_Integrador/Mundi.cs
+++ TP_Integrador/Mundi.cs
@@ -14,35 +14,12 @@ namespace TP_Integrador
 
         private Nodo[,] matriz;
 
-        /// Matriz de distancias
-
-        private int[,] distancia;
-
-        /// cola donde se atenderá a cada uno de los nodos del laberinto
-
-        private Queue<Nodo> cola;
-
-        /// se asume que los nodos adyacentes a un vertice son 4, y estan dados por el que esta arriba(Norte), abajo(Sur), este(Derecha), oeste(izquieda)
-        /// <b>dx</b> es la variacion en la columna
-
-        private int[] dx = { -1, 0, 1, 0 };
-
-        /// dy es la variacion en la fila para los vertices adyacentes, recuerde que ambos, tanto dy y dx combinados en la misma posicion forman el vertice adyacente aumentandole el valor
-        /// almacenado en el vector
-
-        private int[] dy = { 0, -1, 0, 1 };
-
-        /// inicio es el vertice de partida
-        /// fin es el vertice objetivo o salida
-
-        private Nodo inicio, fin;
-
         /// n es el tamaño del mundo
 
         private int n;
 
-
         private List<Cuartel> cuarteles;
+
 
         private List<SitioReciclaje> sitiosReciclaje;
         /// Constructor 
@@ -50,10 +27,8 @@ namespace TP_Integrador
         public Mundi()
         {
             Console.WriteLine("Introduzca el tamaño del Mundo");
-            n = int.Parse(Console.ReadLine());
-            matriz = new Nodo[n+2, n+2]; //se suma +2,para permitir realiazr consultas adyacentes y no desborde la matriz
-            distancia = new int[n+2, n+2];//idem anterior caso
-            cola = new Queue<Nodo>();
+            this.n = int.Parse(Console.ReadLine());
+            matriz = new Nodo[n+2, n+2]; //se suma +2,para permitir realiazr consultas adyacentes y no desborde la matriz     
             cuarteles = new List<Cuartel>();
             sitiosReciclaje = new List<SitioReciclaje> ();
 
@@ -72,7 +47,7 @@ namespace TP_Integrador
             for (int i = 1; i <= n; i++)
             {
 
-                char tipo;
+               
                 //recorremos cada carecter de la cadena leida
                 for (int j = 1; j <= n; j++)
                 {
@@ -85,6 +60,13 @@ namespace TP_Integrador
 
                     this.matriz[i, j] = nodo;
                 }
+            }
+
+            MapaTerrestre mapaT = new MapaTerrestre(this.matriz, this.n, this.cuarteles, this.sitiosReciclaje); /// Creamos un mapa a partir de nuestro mundo terrestre y otro aereo
+            MapaAereo mapaA = new MapaAereo(this.matriz, this.n, this.cuarteles, this.sitiosReciclaje);
+            foreach (Cuartel c in this.cuarteles)
+            {
+                c.asignarMapas(mapaT,mapaA); //a cada cuartel le damos un mapa del mundo
             }
 
         }
@@ -122,7 +104,7 @@ namespace TP_Integrador
                 case 4: terreno = new Vertedero();
                     break;
                 case 5: terreno = new Lago();
-                    break;
+                    break; 
                 case 6: terreno = new VertederoElectronico();
                     break;
                 case 7: terreno = new Cuartel(i,j);
@@ -136,89 +118,7 @@ namespace TP_Integrador
         }
 
 
-        public Stack<Nodo> devolverCamino(int inicioF, int inicioC, int finF, int finC) {
 
-            setInicio(inicioF, inicioC);
-            setFin(finF, finC);
-
-            recorrido();
-            Stack<Nodo> pila = new Stack<Nodo>();
-
-            Nodo actual = fin;
-            pila.Push(actual);
-            while (actual != null)
-            {
-
-                actual = actual.getAnterior();
-                if (actual != null)
-                {
-                    pila.Push(actual);
-                }
-            }
-
-            return pila;
-        }
-
-        private void setInicio(int i, int j)
-        {
-            matriz[i, j].setFila(i);
-            matriz[i, j].setColumna(j);
-            this.inicio = matriz[i, j];
-        }
-        private void setFin(int i, int j)
-        {
-            matriz[i, j].setFila(i);
-            matriz[i, j].setColumna(j);
-            this.fin = matriz[i, j];
-        }
-
-        private void recorrido()
-        {
-            
-
-            //iniciamos la distnaica del nodo inicio en cero
-            distancia[inicio.getFila(), inicio.getColumna()] = 0;
-
-            //marcamos el nodo como visitado
-            matriz[inicio.getFila(), inicio.getColumna()].setVisitado(true);
-
-            //ingresamo el nodo en la cola
-            cola.Enqueue(inicio);
-
-            //mientras que la cola no este vacia
-            while (cola.Count != 0)
-            {
-                //sacamos un nodo de la cola (el primero)
-                Nodo nodoActual = cola.Dequeue();
-                //recorremos los nodos adyacentes al nodoActualS
-                //se recorren 4 posibles nodos adycentes, arriba, abajo, derecha e izquierda
-                for (int i = 0; i < 4; i++)
-                {
-                    //dy y dx nos ayudan a solamente sumar +1,-1 o 0 tanto en fila o columna para los cuatro vertices adycentes
-                    Nodo nodoAdy = matriz[nodoActual.getFila() + dy[i], nodoActual.getColumna() + dx[i]];
-                    // si el nodo no es null , no ha sido visitado... si es un camino valido o es la salida
-                    if (nodoAdy != null && !nodoAdy.isVisitado() && (nodoAdy.getTipo() == 'X' || nodoAdy == this.fin))
-                    {
-                        //marcamos como visitado
-
-                        nodoAdy.setVisitado(true);
-                        //incrementamos su distancia
-                        distancia[nodoAdy.getFila(), nodoAdy.getColumna()] = distancia[nodoActual.getFila(), nodoActual.getColumna()] + 1;
-                        //guardamos la referencia a su nodo anterior para trazar la ruta
-                        nodoAdy.setAnterior(nodoActual);
-                        //ingresamo el nuevo nodo descubierto a la cola
-                        cola.Enqueue(nodoAdy);
-
-                        //si hemos llegado a la salida o al objetivo... no terminamos de recorrer TODO el grafo, y la paramos ahi
-                        if (nodoAdy == this.fin)
-                        {
-
-                            break;
-                        }
-                    }
-                }
-            }
-        }
 
         public void imprimirMundi()
         {
@@ -231,6 +131,26 @@ namespace TP_Integrador
                 }
             }
 
+        }
+
+        public Nodo[,] getMatrizNodos()
+        {
+            return this.matriz;
+        }
+
+        public int getTamanioMundo()
+        {
+            return this.n;
+        }
+
+        public List<Cuartel> getCuarteles()
+        {
+            return this.cuarteles;
+        }
+
+        public List<SitioReciclaje> GetSitioReciclajes()
+        {
+            return this.sitiosReciclaje;
         }
 
     }
