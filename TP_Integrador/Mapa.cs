f5be3cfc64810ceded69984fcff54330d8835eec
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-27 22:13:24 -0300

    Update Mapa.cs

--- TP_Integrador/Mapa.cs
+++ TP_Integrador/Mapa.cs
@@ -1,4 +1,4 @@
-using System;
+sing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +14,20 @@ namespace TP_Integrador
         protected Queue<Nodo> cola;
         protected List<Cuartel> cuarteles;
         protected List<SitioReciclaje> sitiosReciclaje;
+        protected List<Localizacion> vertederos;
 
-        public Mapa(Nodo[,] matriz, int n , List<Cuartel> cuarteles, List<SitioReciclaje> sReciclaje)
+        public Mapa(Nodo[,] matriz, int n , List<Cuartel> cuarteles, List<SitioReciclaje> sReciclaje, List<Localizacion> vertederos)
         {
             this.matriz = matriz;
             distancia = new int[n + 2, n+ 2];
             cola = new Queue<Nodo>();
             this.cuarteles = cuarteles;
             this.sitiosReciclaje = sReciclaje;
+            this.vertederos = vertederos;
         }
 
+        public List<Localizacion> getVertederos() { return this.vertederos; }
+        public List<SitioReciclaje> getSitiosReciclaje() { return this.sitiosReciclaje; }
+
     }
+}
