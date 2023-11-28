0eb83a08f2d8f1f92080ba580daa1c14943637ce
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-27 22:19:18 -0300

    Create EstadoLogicoOp.cs

---
+++ TP_Integrador/EstadoLogicoOp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    public enum EstadoLogicoOp
+    {
+        BuenEstado,
+        StandBy,
+        Inactivo
+    }
+}
