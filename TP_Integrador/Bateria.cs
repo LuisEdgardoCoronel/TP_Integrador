dd11b5362b5018a8ff495d66a1bc063db2a9682c
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-27 22:09:01 -0300

    Update Bateria.cs

--- TP_Integrador/Bateria.cs
+++ TP_Integrador/Bateria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +8,25 @@ namespace TP_Integrador
 {
     internal class Bateria
     {
-        private int bateriaMaxima;
-        private int bateriaActual;
+        private TamañoBateria tipoBateria;
+        private double bateriaMaxima;
+        private double bateriaActual;
         public EstadoBateria estado;
 
 
 
         public Bateria(TamañoBateria bateriaMaxima)
         {
-            this.bateriaMaxima = (int)bateriaMaxima;
-            this.bateriaActual = (int)bateriaMaxima; // Inicializar la carga actual como la capacidad máxima al principio
+            this.tipoBateria = bateriaMaxima;
+            this.bateriaMaxima = (double)bateriaMaxima;
+            this.bateriaActual = (double)bateriaMaxima; // Inicializar la carga actual como la capacidad máxima al principio
             this.estado = EstadoBateria.BuenEstado;
         }
 
 
 
 
-        public int ObtenerCargaActual()
+        public double ObtenerCargaActual()
         {
             return bateriaActual;
         }
@@ -35,7 +37,7 @@ namespace TP_Integrador
 
         public void CargarBateria(int cantidad)
         {
-            if (cantidad > 0 && bateriaActual + cantidad <= bateriaMaxima)
+            if ((cantidad > 0) && (bateriaActual + cantidad <= bateriaMaxima))
             {
                 bateriaActual += cantidad;
             }
@@ -68,36 +70,59 @@ namespace TP_Integrador
             bateriaActual = bateriaMaxima;
         }
 
-
-        
-
-
-
-
-        public void BateriaDañada()
+        public void DescargaPorMovimiento(double velocidad)
         {
-            this.estado = EstadoBateria.Dañada;
+            
+                if (estado == EstadoBateria.Perforada)
+                {
+                    bateriaActual -= 1000 / (5 * velocidad);
+                }
+                else
+                {
+                    bateriaActual -= 1000 / velocidad;
+                }
+            
+            
+
+        }
+
+
+        //carga reducida por vertedero electronico
+        public void ReducirCarga()
+        {
+            bateriaMaxima *= 0.2;
+            if (bateriaActual > bateriaMaxima) bateriaActual = bateriaMaxima;
         }
 
 
 
 
 
+        public void SetEstadoBateria(EstadoBateria estado)
+        {
+            this.estado = estado;
+        }
 
-        public EstadoBateria SetEstadoBateria()
+
+
+
+        public EstadoBateria GetEstadoBateria()
         {
             return this.estado;
         }
 
 
 
+        public TamañoBateria GetTamañoBateria()
+        {
+            return this.tipoBateria;
+        }
 
 
-
-
-
-
-
+        public double getBateriaActual()
+        {
+            return this.bateriaActual;
+        }
 
 
     }
