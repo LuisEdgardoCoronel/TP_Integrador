8798d496ee9c549d2305247df3d26e824ec82560
Hualpa7 <142543744+Hualpa7@users.noreply.github.com>
2023-11-29 15:38:34 -0300

    Version_"Estable"
    - Falta fronted
    - Falta logica de los mapas

---
+++ Localizacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class Localizacion
+    {
+        
+
+        public int filaProperty { get;  set; }
+        public int columnaProperty { get;  set; }
+
+
+        public Localizacion() { }
+
+        [JsonConstructor]
+        public Localizacion(int filaProperty, int columnaProperty)
+        {
+            this.filaProperty = filaProperty;
+            this.columnaProperty = columnaProperty;
+            
+           
+        }
+
+
+
+
+        public bool equals(Localizacion local2) ///metodo nuevo para comparar localizaciones
+        {
+            return (this.filaProperty== local2.filaProperty && this.columnaProperty == local2.columnaProperty);
+        }
+        
+
+
+    }
+}
---
+++ SkyNet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    internal class SkyNet
+    {
+        public Mundi mundoProperty { get;  set; }
+
+
+        public SkyNet()
+        {
+            mundoProperty = new Mundi();
+           
+        }
+
+
+
+
+
+        public void guardarInfo()
+        {
+
+            string path = Directory.GetCurrentDirectory(); 
+            path += "\\data";
+            string filename = "\\archivoPrueba.json";
+            Directory.CreateDirectory(path);
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true, // Hacer que la serialización sea insensible a mayúsculas y minúsculas
+                                                    // Otras opciones...
+            };
+            string data = JsonSerializer.Serialize(mundoProperty,options);
+
+            File.WriteAllText(path + filename, data);
+        }
+
+        public void leerInfo()
+        {
+            string path = Directory.GetCurrentDirectory();
+            path += "\\data";
+            string filename = "\\archivoPrueba.json";
+            string data = File.ReadAllText(path + filename);
+            try
+            {
+                  mundoProperty = JsonSerializer.Deserialize<Mundi>(data);
+
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine("Error al leer el archivo." + e.ToString());
+                
+            }   
+        }
+
+
+    }
+
+   
+
+  
+}
