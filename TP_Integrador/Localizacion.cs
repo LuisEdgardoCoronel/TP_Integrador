c244f54135ad4898b83c89bca19a388d18c3feb0
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-12-01 01:04:03 -0300

    Update Localizacion.cs
    actualizacion de metodos y agregamos properties

--- TP_Integrador/Localizacion.cs
+++ TP_Integrador/Localizacion.cs
@@ -2,54 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
     internal class Localizacion
     {
-        private int fila,columna;
-
-        public int filaProperty { get; private set; }
-        public int columnaProperty { get; private set; }
-        // probar
-        public string nombreProperty { get; private set; }
+        public int filaProperty { get; set; }
+        public int columnaProperty { get; set; }
 
 
-        public Localizacion(int filaProperty, int columnaProperty, string nombreProperty)
+        //public Localizacion() { }
+
+        [JsonConstructor]
+        public Localizacion(int filaProperty, int columnaProperty)
         {
-            this.columnaProperty = columnaProperty;
             this.filaProperty = filaProperty;
-            this.nombreProperty = nombreProperty;
-        }
+            this.columnaProperty = columnaProperty;
 
-
-        public Localizacion(int f, int c) {
-        //    this.fila = f;
-        //    this.columna = c;
-            this.columnaProperty = c;
-            this.filaProperty = f;
-            this.nombreProperty = "ubicacion [" + f + "][" + c +"]";
-        }
-
-         public int getFila() { return fila; }
-
-         public void setFila(int fila)
-        {
-            this.fila = fila;
-        }
-        public int getColumna() { return columna;}
-
-        public void setColumna(int columna)
-        {
-            this.columna = columna;
         }
 
         public bool equals(Localizacion local2) ///metodo nuevo para comparar localizaciones
         {
-            return (this.fila == local2.getFila() && this.columna == local2.getColumna());
+            return (this.filaProperty == local2.filaProperty && this.columnaProperty == local2.columnaProperty);
         }
-        
+
+
+
 
 
     }
