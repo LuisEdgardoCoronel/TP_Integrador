a33064c3882e35bc79400fbd220dff8a0a83943c
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-12-01 01:02:07 -0300

    Update Bateria.cs
    removimos metodos y agregamos properties

--- TP_Integrador/Bateria.cs
+++ TP_Integrador/Bateria.cs
@@ -2,19 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
     internal class Bateria
     {
-        private TamañoBateria tipoBateria;
-        private double bateriaMaxima;
-        private double bateriaActual;
-        public EstadoBateria estado;
+        public TamañoBateria tipoBateria { get; set; }
+        public double bateriaMaxima { get; set; }
+        public double bateriaActual { get; set; }
+        public EstadoBateria estado { get; set; }
+
+
+        [JsonConstructor]
+        public Bateria(TamañoBateria tipoBateria, double bateriaMaxima, double bateriaActual, EstadoBateria estado)
+        {
+            this.tipoBateria = tipoBateria;
+            this.bateriaMaxima = bateriaMaxima;
+            this.bateriaActual = bateriaActual;
+            this.estado = estado;
 
 
 
+        }
         public Bateria(TamañoBateria bateriaMaxima)
         {
             this.tipoBateria = bateriaMaxima;
@@ -22,16 +33,6 @@ namespace TP_Integrador
             this.bateriaActual = (double)bateriaMaxima; // Inicializar la carga actual como la capacidad máxima al principio
             this.estado = EstadoBateria.BuenEstado;
         }
-
-
-
-
-        public double ObtenerCargaActual()
-        {
-            return bateriaActual;
-        }
-
-
 
 
 
@@ -47,9 +48,6 @@ namespace TP_Integrador
             }
         }
 
-
-
-
         public void DescargarBateria(int cantidad)
         {
             if (cantidad > 0 && bateriaActual - cantidad >= 0)
@@ -62,9 +60,6 @@ namespace TP_Integrador
             }
         }
 
-
-
-
         public void RecargarBateriaCompleta()
         {
             bateriaActual = bateriaMaxima;
@@ -72,17 +67,17 @@ namespace TP_Integrador
 
         public void DescargaPorMovimiento(double velocidad)
         {
-            
-                if (estado == EstadoBateria.Perforada)
-                {
-                    bateriaActual -= 1000 / (5 * velocidad);
-                }
-                else
-                {
-                    bateriaActual -= 1000 / velocidad;
-                }
-            
-            
+
+            if (estado == EstadoBateria.Perforada)
+            {
+                bateriaActual -= 1000 / (5 * velocidad);
+            }
+            else
+            {
+                bateriaActual -= 1000 / velocidad;
+            }
+
+
 
         }
 
@@ -93,37 +88,6 @@ namespace TP_Integrador
             bateriaMaxima *= 0.2;
             if (bateriaActual > bateriaMaxima) bateriaActual = bateriaMaxima;
         }
-
-
-
-
-
-        public void SetEstadoBateria(EstadoBateria estado)
-        {
-            this.estado = estado;
-        }
-
-
-
-
-        public EstadoBateria GetEstadoBateria()
-        {
-            return this.estado;
-        }
-
-
-
-        public TamañoBateria GetTamañoBateria()
-        {
-            return this.tipoBateria;
-        }
-
-
-        public double getBateriaActual()
-        {
-            return this.bateriaActual;
-        }
-
 
     }
 
