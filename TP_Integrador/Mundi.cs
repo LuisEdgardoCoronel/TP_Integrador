ba5c22b415e285db2ff2d79e4ef458eb1ca7e4a0
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-12-01 01:04:42 -0300

    Update Mundi.cs
    corregimos los errores en metodos

--- TP_Integrador/Mundi.cs
+++ TP_Integrador/Mundi.cs
@@ -2,75 +2,80 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
     internal class Mundi
     {
-      
+
 
         /// El atributo matriz representa el laberinto  por medio de Vertices que en realidad le corresponden a la clase Nodo
 
-        private Nodo[,] matriz;
+        private Nodo[,] matriz { get; set; }
 
-        /// n es el tamaño del mundo
+        public List<Nodo> terrenos { get; set; }  // almacena los datos de matriz para poder serializar
 
-        private int n;
+        public int n { get; set; } //n es el tamaño del mundo
 
-        private List<Cuartel> cuarteles { get; set; }
+        public List<Cuartel> cuarteles { get; set; } //lista de cuarteles del mundo
+
+        public List<SitioReciclaje> sitiosReciclaje { get; set; } //lista de sitios de reciclaje del mundo
+
+        public List<Localizacion> vertederos { get; set; } //lista de vertederos del mundo (se decidio guardar las
+                                                           //localizaciones de los vertederos para facilitar la
+                                                           //ejecucion de ciertos metodos)
 
 
-        private List<SitioReciclaje> sitiosReciclaje;
-
-        private List<Localizacion> vertederos; 
-        /// Constructor 
+        [JsonConstructor]
+        public Mundi(List<Nodo> terrenos, int n, List<Cuartel> cuarteles, List<SitioReciclaje> sitiosReciclaje, List<Localizacion> vertederos)
+        {
+            this.n = n;
+            this.terrenos = terrenos;
+            this.cuarteles = cuarteles;
+            this.sitiosReciclaje = sitiosReciclaje;
+            this.vertederos = vertederos;
+            this.matriz = Convert.convertListToMatrice(terrenos);
+        }
 
         public Mundi()
         {
             Console.WriteLine("Introduzca el tamaño del Mundo");
             this.n = int.Parse(Console.ReadLine());
-            matriz = new Nodo[n+2, n+2]; //se suma +2,para permitir realiazr consultas adyacentes y no desborde la matriz     
+            matriz = new Nodo[n + 2, n + 2]; //se suma +2,para permitir realiazr consultas adyacentes y no desborde la matriz     
             cuarteles = new List<Cuartel>();
-            sitiosReciclaje = new List<SitioReciclaje> ();
+            sitiosReciclaje = new List<SitioReciclaje>();
             vertederos = new List<Localizacion>();
             cargarMundi();
+            terrenos = Convert.convertMatriceToList(this.matriz);
         }
-
-       
-
-
 
         public void cargarMundi()
         {
-            
+
             ///iniciamos en el indice 1, dejando el indice 0 para hacer consultas en las adyacencias
-         
+
 
             for (int i = 1; i <= n; i++)
             {
 
-               
-                //recorremos cada carecter de la cadena leida
                 for (int j = 1; j <= n; j++)
                 {
                     Nodo nodo = new Nodo();
-                    Terreno tipoTerreno = terrenoRandom(i,j);
-                    nodo.setTipo(tipoTerreno);
-                    nodo.setFila(i);
-                    nodo.setColumna(j);
-                    nodo.setVisitado(false);
+                    Terreno tipoTerreno = terrenoRandom(i, j);
+                    nodo.tipo = tipoTerreno;
+                    nodo.fila = i;
+                    nodo.columna = j;
+                    nodo.visitado = false;
 
                     this.matriz[i, j] = nodo;
+
                 }
             }
 
-            MapaTerrestre mapaT = new MapaTerrestre(this.matriz, this.n, this.cuarteles, this.sitiosReciclaje,this.vertederos); /// Creamos un mapa a partir de nuestro mundo terrestre y otro aereo
-            MapaAereo mapaA = new MapaAereo(this.matriz, this.n, this.cuarteles, this.sitiosReciclaje,this.vertederos);
-            foreach (Cuartel c in this.cuarteles)
-            {
-                c.asignarMapas(mapaT,mapaA); //a cada cuartel le damos un mapa del mundo
-            }
+            cambiarTerreno(3, 5); // luego de cargar el terreno, se lo limpia para crear un cuartel(como maximo 3)
+                                  // o un sitio de reciclaje (como maximo 5).
 
         }
 
@@ -79,51 +84,46 @@ namespace TP_Integrador
             Random rnd = new Random();
             Terreno terreno;
 
-            do
-            {
-                int tipo = rnd.Next(0, 9);
-                terreno = creaTerreno(tipo,i,j);
-              
-            } while ((terreno is Cuartel && this.cuarteles.Count()>=3)||(terreno is SitioReciclaje && this.sitiosReciclaje.Count()>=5));
+            int tipo = rnd.Next(0, 7);
+            terreno = creaTerreno(tipo, i, j);
 
-            if (terreno is Cuartel) this.cuarteles.Add((Cuartel)terreno);
-            if (terreno is SitioReciclaje) this.sitiosReciclaje.Add((SitioReciclaje)terreno);
-            Localizacion local = new Localizacion(i, j);
-            if (terreno is Vertedero) this.vertederos.Add(local);
+            if (terreno is Vertedero) this.vertederos.Add(new Localizacion(i, j));
+
             return terreno;
         }
 
-        private Terreno creaTerreno(int tipo,int i,int j) //metodo para modularizar la manera de crear un tipo de terreno a partir de 
+
+        private Terreno creaTerreno(int tipo, int i, int j) //metodo para modularizar la manera de crear un tipo de terreno a partir de 
         {                                                 //un numero random y 2 enteros necesarios para crear cuartel y sitioreciclaje
             Terreno terreno;
             switch (tipo)
             {
-                case 0: terreno = new Baldio();
+                case 0:
+                    terreno = new Baldio();
                     break;
-                case 1: terreno = new Planicie();
+                case 1:
+                    terreno = new Planicie();
                     break;
-                case 2: terreno = new Bosque();
+                case 2:
+                    terreno = new Bosque();
                     break;
-                case 3: terreno = new Urbano();
+                case 3:
+                    terreno = new Urbano();
                     break;
-                case 4: terreno = new Vertedero();
+                case 4:
+                    terreno = new Vertedero();
                     break;
-                case 5: terreno = new Lago();
-                    break; 
-                case 6: terreno = new VertederoElectronico();
+                case 5:
+                    terreno = new Lago();
                     break;
-                case 7: terreno = new Cuartel(i,j);
+                case 6:
+                    terreno = new VertederoElectronico();
                     break;
-                case 8: terreno = new SitioReciclaje(i,j);
-                    break;
-                    default: return null;
-            } 
+                default: return null;
+            }
 
             return terreno;
         }
-
-
-
 
         public void imprimirMundi()
         {
@@ -132,30 +132,68 @@ namespace TP_Integrador
                 Console.WriteLine();
                 for (int j = 1; j <= n; j++)
                 {
-                    Console.Write((matriz[i, j]).getTipo());
+                    Console.Write((matriz[i, j]).GetType().Name);
                 }
             }
 
         }
 
-        public Nodo[,] getMatrizNodos()
+        private void cambiarTerreno(int cantidadMaxCuartel, int cantidadMaxReciclaje) //cambia el terreno por un cuartel
+                                                                                      // o por un sitio de reciclaje
+        {
+
+            crearCuartel();
+            crearSitioReciclaje();
+            Random rnd = new Random();
+
+            for (int i = 0; i < cantidadMaxCuartel - 1; i++)
+            {
+
+                int posbilidad = rnd.Next(0, 2);
+
+                if (posbilidad == 0)
+                {
+                    crearCuartel();
+
+                }
+            }
+
+            for (int i = 0; i < cantidadMaxReciclaje - 1; i++)
+            {
+
+                int posbilidad = rnd.Next(0, 2);
+
+                if (posbilidad == 0)
+                {
+                    crearSitioReciclaje();
+
+                }
+            }
+        }
+
+        private void crearCuartel()
+        {
+            Random rnd = new Random();
+            int randomI = rnd.Next(1, this.n + 1);
+            int randomJ = rnd.Next(1, this.n + 1);
+            Terreno terreno = new Cuartel(randomI, randomJ);
+            this.matriz[randomI, randomJ].tipo = terreno;
+            this.cuarteles.Add((Cuartel)terreno);
+        }
+
+        private void crearSitioReciclaje()
+        {
+            Random rnd = new Random();
+            int randomI = rnd.Next(1, this.n + 1);
+            int randomJ = rnd.Next(1, this.n + 1);
+            Terreno terreno = new SitioReciclaje(randomI, randomJ);
+            this.matriz[randomI, randomJ].tipo = terreno;
+            this.sitiosReciclaje.Add((SitioReciclaje)terreno);
+        }
+
+        public Nodo[,] getMatrizNodo()
         {
             return this.matriz;
-        }
-
-        public int getTamanioMundo()
-        {
-            return this.n;
-        }
-
-        public List<Cuartel> getCuarteles()
-        {
-            return this.cuarteles;
-        }
-
-        public List<SitioReciclaje> GetSitioReciclajes()
-        {
-            return this.sitiosReciclaje;
         }
 
     }
