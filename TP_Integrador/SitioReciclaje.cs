3f8df24096c06e237044a11e6fb4761216d950e9
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-12-06 22:28:47 -0300

    Update SitioReciclaje.cs
    agregue metodo faltante

--- TP_Integrador/SitioReciclaje.cs
+++ TP_Integrador/SitioReciclaje.cs
@@ -2,16 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TP_Integrador
 {
     internal class SitioReciclaje : Terreno
     {
-        private int fila, columna;
-        public SitioReciclaje(int f, int c) { 
-            this.fila = f;
-            this.columna = c;
+        public int fila { get; set; }
+        public int columna { get; set; }
+
+
+        [JsonConstructor]
+        public SitioReciclaje(int fila, int columna)
+        {
+            this.fila = fila;
+            this.columna = columna;
+        }
+
+        public Localizacion getLocalizacion()
+        {
+            return new Localizacion(fila, columna);
         }
     }
 }
